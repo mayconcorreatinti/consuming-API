edc48746bee94caa324a01b2993e4ed601040506
mayconcorreatinti <maycon326754@gmail.com>
2025-05-24 15:47:13 -0300

    error and exception handling and adding minor improvements

--- consuming_api/main.py
+++ consuming_api/main.py
@@ -1,33 +1,36 @@
-import requests
 import json
+import requests
+from typing import List
 from googletrans import Translator
+from requests.exceptions import RequestException
 
 
-def request_get() -> str:
+def request_get() -> dict:
     url = "https://last-airbender-api.fly.dev/api/v1/characters"
-    response = requests.get(url)
+    try:
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
+        return response.json()
+    except RequestException as e:
+         raise Exception(f"Failed to fetch data: {e}")
 
-    if response.status_code == 200:
-        return json.dumps(response.json(),indent=4)
-    else:
-        return f"error {response.status_code}"
-    
 
-def translate_text() -> list:
-    name_affiliation_translated=[]
-
+def translate_text():
+    data=request_get()
     translator = Translator()
-
-    json_data=json.loads(request_get())
-
-    for a in json_data:
-        name_translated = translator.translate(a.get("name"),dest="pt")
-        affiliation_translated = translator.translate(a.get("affiliation") if "affiliation" in a else 'N',dest="pt")
-        name_affiliation_translated.append([name_translated.text,affiliation_translated.text])
-
-    return name_affiliation_translated
+    result = []
+    for character in data:
+        try:
+            name = translator.translate(character.get("name", ""), dest="pt").text
+            affiliation = translator.translate(character.get("affiliation", "N"), dest="pt").text
+            result.append([name, affiliation])
+        except Exception as e:
+            raise Exception(f"Translation error: {e}")
+    return result
 
 
 if __name__ == "__main__":
     print(request_get())
     print(translate_text())
+
+
