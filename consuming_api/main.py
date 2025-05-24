d89aa407e82711e04767e02a79d7daee7d803bc0
mayconcorreatinti <maycon326754@gmail.com>
2025-05-24 12:31:29 -0300

    translating character names and affiliations with GoogleTrans

--- consuming_api/main.py
+++ consuming_api/main.py
@@ -1,14 +1,33 @@
 import requests
 import json
+from googletrans import Translator
 
-def request_get():
+
+def request_get() -> str:
     url = "https://last-airbender-api.fly.dev/api/v1/characters"
     response = requests.get(url)
+
     if response.status_code == 200:
         return json.dumps(response.json(),indent=4)
     else:
         return f"error {response.status_code}"
     
 
+def translate_text() -> list:
+    name_affiliation_translated=[]
+
+    translator = Translator()
+
+    json_data=json.loads(request_get())
+
+    for a in json_data:
+        name_translated = translator.translate(a.get("name"),dest="pt")
+        affiliation_translated = translator.translate(a.get("affiliation") if "affiliation" in a else 'N',dest="pt")
+        name_affiliation_translated.append([name_translated.text,affiliation_translated.text])
+
+    return name_affiliation_translated
+
+
 if __name__ == "__main__":
     print(request_get())
+    print(translate_text())
