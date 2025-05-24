e824ff97c8d8bbc85cb509ba678a8a2f6c5ca5a3
mayconcorreatinti <maycon326754@gmail.com>
2025-05-24 08:23:50 -0300

    converting python object to json string

--- consuming_api/main.py
+++ consuming_api/main.py
@@ -1,11 +1,11 @@
 import requests
-
+import json
 
 def request_get():
     url = "https://last-airbender-api.fly.dev/api/v1/characters"
     response = requests.get(url)
     if response.status_code == 200:
-        return response.json()
+        return json.dumps(response.json(),indent=4)
     else:
         return f"error {response.status_code}"
     
